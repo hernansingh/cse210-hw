cf9e1229b02c87cbb897fe2a3ba20cfdeb5211a3
hernanjoelsingh <hernanjoelsingh@gmail.com>
2024-11-09 11:03:04 -0300

    Prep 3 Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+    Random randomGenerator = new Random();
+    int random = randomGenerator.Next(1, 100);
+
+    while (true)
+    {
+
+        Console.WriteLine("Guees your random number ");
+        int guess = int.Parse(Console.ReadLine());
+
+        if (random < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else if (random > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else
+        {
+            Console.WriteLine("You guessed it!");
+            break;
+        }
+    }
+
     }
 }
