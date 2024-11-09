17ee268d69ae8db8df97f7b6ac27fdaa95123b60
hernanjoelsingh <hernanjoelsingh@gmail.com>
2024-11-09 12:40:43 -0300

    Prep 4 Finished!!

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Globalization;
+
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+    
+    int total = 0;
+    int trys = 0;
+    int largest = int.MinValue;
+    List<string> listNumbers = new List<string>();
+
+    while (true)
+    {
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.WriteLine("Enter number: ");
+        int numbers = int.Parse(Console.ReadLine());
+
+        if (numbers != 0)
+        {
+            listNumbers.Add(numbers.ToString());
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    foreach (string number in listNumbers)
+    {
+        total += int.Parse(number);
+        trys = trys + 1;
+
+        int currentNumber = 0;
+        if (int.TryParse(number, out currentNumber))
+        {
+            if (currentNumber > largest)
+            {
+                largest = currentNumber;
+            }
+        }
+    }
+    
+    float avg = (float)total / trys;
+
+    Console.WriteLine($"The sum is: {total}");
+    Console.WriteLine($"The average is: {avg}");
+    Console.WriteLine($"The largest number is: {largest}");
     }
 }
